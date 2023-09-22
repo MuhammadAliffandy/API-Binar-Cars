f53dbe272bafac393dcd7d34619b44434b4607a2
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2023-09-22 18:38:30 +0700

    update configuration vercel and more code

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use('/',routes);
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: "Ping successfully" });
+}).get('/tes', (req, res) => {
+    res.status(200).json({ message: "Ping successfully" });
 }).all('*', (req, res) => {
     res.status(404).json({ message : 'method and endpoint its not available' })
 })
