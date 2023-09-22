da891e1fb22fab57ad82196e47c698935b31663e
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2023-09-22 19:01:14 +0700

    update configuration vercel and more code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const routes = require("./src/routes/route")
+const routes = require("./src/routes/route.js")
 
 app.use(express.json());
 app.use('/',routes);
