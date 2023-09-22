5b350cab70387be5e27f124b5cc2a0e31ecad745
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2023-09-22 10:45:29 +0700

    update configuration vercel and more code

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,7 +2,9 @@ const express = require('express' );
 const router = express.Router();
 
 const car = require('../api/cars');
+const tes = require('../api/test');
 
 router.use('/cars', car);
+router.use('/test', tes);
 
 module.exports = router;
